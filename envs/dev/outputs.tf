657fb0baffd59109430a5b6b8584f7eea4ad0fd2
danil <dannsk@inbox.ru>
2026-06-24 23:25:31 +0700

    test

--- envs/dev/outputs.tf
+++ envs/dev/outputs.tf
@@ -1,4 +1,5 @@
 output "bastion_public_ip" { value = module.compute.bastion_public_ip }
+output "bastion_private_ip" { value = module.compute.bastion_private_ip }
 output "load_balancer_ip" { value = module.loadbalancer.lb_public_ip }
 output "web_private_ips" { value = module.compute.web_private_ips }
 output "db_host" { value = module.database.db_host }
