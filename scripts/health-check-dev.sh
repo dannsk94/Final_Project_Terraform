45387e377476a3968f0991e5515ccb01039c7782
danil <dannsk@inbox.ru>
2026-06-28 00:18:28 +0700

    test

--- scripts/health-check-dev.sh
+++ scripts/health-check-dev.sh
@@ -3,20 +3,19 @@ set -e
 
 # Проверка балансировщика (dev)
 LB_IP=$(terraform output -raw load_balancer_ip)
-echo "Проверяем балансировщик (пинг): $LB_IP"
+echo "Проверяем балансировщик (HTTP): http://$LB_IP"
 sleep 60
-if ping -c 2 -W 3 "$LB_IP" > /dev/null 2>&1; then
-  echo "Балансировщик пингуется успешно"
+if curl -s -o /dev/null --max-time 10 http://$LB_IP; then
+  echo "Балансировщик отвечает на HTTP"
 else
-  echo "Балансировщик не пингуется, пересоздаём Floating IP..."
-  OLD_IP=$(terraform output -raw load_balancer_ip)
-  openstack floating ip delete "$OLD_IP"
-  NEW_IP=$(openstack floating ip create internet -c floating_ip_address -f value)
-  openstack floating ip set --port "$(openstack loadbalancer show lab7-dev-lb -c vip_port_id -f value)" "$NEW_IP"
+  echo "Балансировщик не отвечает, пересоздаём Floating IP..."
+  openstack --os-cloud vkcs floating ip delete "$LB_IP" || echo "Старый IP не удалён, создаём новый"
+  NEW_IP=$(openstack --os-cloud vkcs floating ip create internet -c floating_ip_address -f value)
+  openstack --os-cloud vkcs floating ip set --port "$(openstack --os-cloud vkcs loadbalancer show lab7-dev-lb -c vip_port_id -f value)" "$NEW_IP"
   echo "Новый Floating IP балансировщика: $NEW_IP"
 fi
 
-# Проверка бастиона (dev)
+# Проверка бастиона
 BASTION_IP=$(terraform output -raw bastion_public_ip)
 echo "Проверяем бастион (пинг): $BASTION_IP"
 sleep 10
@@ -24,10 +23,9 @@ if ping -c 2 -W 3 "$BASTION_IP" > /dev/null 2>&1; then
   echo "Бастион пингуется успешно"
 else
   echo "Бастион не пингуется, пересоздаём Floating IP..."
-  OLD_IP=$(openstack floating ip list --fixed-ip-address "$(terraform output -raw bastion_public_ip)" -c "Floating IP Address" -f value)
-  openstack floating ip delete "$OLD_IP"
-  NEW_IP=$(openstack floating ip create internet -c floating_ip_address -f value)
-  BASTION_PORT=$(openstack port list --server lab7-dev-bastion -c ID -f value | head -1)
-  openstack floating ip set --port "$BASTION_PORT" "$NEW_IP"
+  openstack --os-cloud vkcs floating ip delete "$BASTION_IP" || echo "Старый IP не удалён, создаём новый"
+  NEW_IP=$(openstack --os-cloud vkcs floating ip create internet -c floating_ip_address -f value)
+  BASTION_PORT=$(openstack --os-cloud vkcs port list --server lab7-dev-bastion -c ID -f value | head -1)
+  openstack --os-cloud vkcs floating ip set --port "$BASTION_PORT" "$NEW_IP"
   echo "Новый Floating IP бастиона: $NEW_IP"
 fi
