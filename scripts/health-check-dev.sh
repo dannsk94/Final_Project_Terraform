cabb797a37f98d10f70ff4d9f71733f1198e2cad
danil <dannsk@inbox.ru>
2026-06-28 00:09:29 +0700

    test

--- scripts/health-check-dev.sh
+++ scripts/health-check-dev.sh
@@ -9,7 +9,7 @@ if ping -c 2 -W 3 "$LB_IP" > /dev/null 2>&1; then
   echo "Балансировщик пингуется успешно"
 else
   echo "Балансировщик не пингуется, пересоздаём Floating IP..."
-  OLD_IP=$(openstack floating ip list --fixed-ip-address "$(terraform output -raw load_balancer_ip)" -c "Floating IP Address" -f value)
+  OLD_IP=$(terraform output -raw load_balancer_ip)
   openstack floating ip delete "$OLD_IP"
   NEW_IP=$(openstack floating ip create internet -c floating_ip_address -f value)
   openstack floating ip set --port "$(openstack loadbalancer show lab7-dev-lb -c vip_port_id -f value)" "$NEW_IP"
