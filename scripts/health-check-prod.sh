4fc1f457e5db64d3187cf5737eec7aa308fbeb80
danil <dannsk@inbox.ru>
2026-06-27 23:54:00 +0700

    test

--- scripts/health-check-prod.sh
+++ scripts/health-check-prod.sh
@@ -9,7 +9,7 @@ if ping -c 2 -W 3 "$LB_IP" > /dev/null 2>&1; then
   echo "Балансировщик пингуется успешно"
 else
   echo "Балансировщик не пингуется, пересоздаём Floating IP..."
-  OLD_IP=$(openstack floating ip list --fixed-ip-address "$(terraform output -raw lb_internal_ip)" -c "Floating IP Address" -f value)
+  OLD_IP=$(openstack floating ip list --fixed-ip-address "$(terraform output -raw load_balancer_ip)" -c "Floating IP Address" -f value)
   openstack floating ip delete "$OLD_IP"
   NEW_IP=$(openstack floating ip create internet -c floating_ip_address -f value)
   openstack floating ip set --port "$(openstack loadbalancer show lab7-prod-lb -c vip_port_id -f value)" "$NEW_IP"
@@ -24,7 +24,7 @@ if ping -c 2 -W 3 "$BASTION_IP" > /dev/null 2>&1; then
   echo "Бастион пингуется успешно"
 else
   echo "Бастион не пингуется, пересоздаём Floating IP..."
-  OLD_IP=$(openstack floating ip list --fixed-ip-address "$(terraform output -raw bastion_private_ip)" -c "Floating IP Address" -f value)
+  OLD_IP=$(openstack floating ip list --fixed-ip-address "$(terraform output -raw bastion_public_ip)" -c "Floating IP Address" -f value)
   openstack floating ip delete "$OLD_IP"
   NEW_IP=$(openstack floating ip create internet -c floating_ip_address -f value)
   BASTION_PORT=$(openstack port list --server lab7-prod-bastion -c ID -f value | head -1)
